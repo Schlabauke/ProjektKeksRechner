5208f677f7e3ee0b07fe66beb018b174b008eb48
Schlabauke <annabelle.jeffre@gmail.com>
2021-11-22 19:20:24 +0100

    ProjektKeksRechner

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,63 +11,70 @@ let women = document.getElementById("women")
 let pal = document.getElementById("pal")
 let outPutKcal = document.getElementById("outputKcal")
 let outPutCookies = document.getElementById("outputCookies")
+let cookieRatio = document.getElementById("cookieRatio")
 
 
 
 let giveMeCookies = () => {
     if (pal.value == .95) {
-        console.log("Wuhuuu")
         if (men.checked === true) {
-            outPutKcal.innerHTML = menCookies() + " Kekse"
-            outPutCookies.innerHTML = menCookies() * .95 + " Kekse"
+            outPutKcal.innerHTML = "Dein Grundumsatz beträgt: " + menCookies() + " kcal"
+            outPutCookies.innerHTML = "Dein Gesamumsatz beträgt: " + menCookies() * .95 + " kcal"
+            cookieRatio.innerHTML = cookieRatioMen() + " Kekse "
         } else {
-            outPutKcal.innerHTML = womenCookies() + " Kekse"
-            outPutCookies.innerHTML = womenCookies() * .95 + " Kekse"
+            outPutKcal.innerHTML = "Dein Grundumsatz beträgt: " + womenCookies() + " kcal"
+            outPutCookies.innerHTML = "Dein Gesamumsatz beträgt: " + womenCookies() * .95 + " kcal"
+            cookieRatio.innerHTML = cookieRatioWomen() + " Kekse "
         }
     } else if (pal.value == 1.2) {
-        console.log("Nur Sitzen und Liegen")
         if (men.checked === true) {
-            outPutKcal.innerHTML = menCookies() + " Kekse"
-            outPutCookies.innerHTML = menCookies() * 1.2 + " Kekse"
+            outPutKcal.innerHTML = "Dein Grundumsatz beträgt: " + menCookies() + " kcal"
+            outPutCookies.innerHTML = "Dein Gesamumsatz beträgt: " + menCookies() * 1.2 + " kcal"
+            cookieRatio.innerHTML = cookieRatioMen() + " Kekse "
         } else {
-            outPutKcal.innerHTML = womenCookies() + " Kekse"
-            outPutCookies.innerHTML = womenCookies() * 1.2 + " Kekse"
+            outPutKcal.innerHTML = "Dein Grundumsatz beträgt: " + womenCookies() + " kcal"
+            outPutCookies.innerHTML = "Dein Gesamumsatz beträgt: " + womenCookies() * 1.2 + " kcal"
+            cookieRatio.innerHTML = cookieRatioWomen() + " Kekse "
         }
     } else if (pal.value == 1.5) {
-        console.log("Yeah!")
         if (men.checked === true) {
-            outPutKcal.innerHTML = menCookies() + " Kekse"
-            outPutCookies.innerHTML = menCookies() * 1.5 + " Kekse"
+            outPutKcal.innerHTML = "Dein Grundumsatz beträgt: " + menCookies() + " kcal"
+            outPutCookies.innerHTML = "Dein Gesamumsatz beträgt: " + menCookies() * 1.5 + " kcal"
+            cookieRatio.innerHTML = cookieRatioMen() + " Kekse "
         } else {
-            outPutKcal.innerHTML = womenCookies() + " Kekse"
-            outPutCookies.innerHTML = womenCookies() * 1.5 + " Kekse"
+            outPutKcal.innerHTML = "Dein Grundumsatz beträgt: " + womenCookies() + " kcal"
+            outPutCookies.innerHTML = "Dein Gesamumsatz beträgt: " + womenCookies() * 1.5 + " kcal"
+            cookieRatio.innerHTML = cookieRatioWomen() + " Kekse "
         }
     } else if (pal.value == 1.7) {
-        console.log("Kekse")
         if (men.checked === true) {
-            outPutKcal.innerHTML = menCookies() + " Kekse"
-            outPutCookies.innerHTML = menCookies() * 1.7 + " Kekse"
+            outPutKcal.innerHTML = "Dein Grundumsatz beträgt: " + menCookies() + " kcal"
+            outPutCookies.innerHTML = "Dein Gesamumsatz beträgt: " + menCookies() * 1.7 + " kcal"
+            cookieRatio.innerHTML = cookieRatioMen() + " Kekse "
         } else {
-            outPutKcal.innerHTML = womenCookies() + " Kekse"
-            outPutCookies.innerHTML = womenCookies() * 1.7 + " Kekse"
+            outPutKcal.innerHTML = "Dein Grundumsatz beträgt: " + womenCookies() + " kcal"
+            outPutCookies.innerHTML = "Dein Gesamumsatz beträgt: " + womenCookies() * 1.7 + " kcal"
+            cookieRatio.innerHTML = cookieRatioWomen() + " Kekse "
         }
     } else if (pal.value == 1.9) {
-        console.log("Puhhh")
         if (men.checked === true) {
-            outPutKcal.innerHTML = menCookies() + " Kekse"
-            outPutCookies.innerHTML = menCookies() * 1.9 + " Kekse"
+            outPutKcal.innerHTML = "Dein Grundumsatz beträgt: " + menCookies() + " kcal"
+            outPutCookies.innerHTML = "Dein Gesamumsatz beträgt: " + menCookies() * 1.9 + " kcal"
+            cookieRatio.innerHTML = cookieRatioMen() + " Kekse "
         } else {
-            outPutKcal.innerHTML = womenCookies() + " Kekse"
-            outPutCookies.innerHTML = womenCookies() * 1.9 + " Kekse"
+            outPutKcal.innerHTML = "Dein Grundumsatz beträgt: " + womenCookies() + " kcal"
+            outPutCookies.innerHTML = "Dein Gesamumsatz beträgt: " + womenCookies() * 1.9 + " kcal"
+            cookieRatio.innerHTML = cookieRatioWomen() + " Kekse "
         }
     } else if (pal.value == 2.2) {
-        console.log("I've must have called a thousand times")
         if (men.checked === true) {
-            outPutKcal.innerHTML = menCookies() + " Kekse"
-            outPutCookies.innerHTML = menCookies() * 2.2 + " Kekse"
+            outPutKcal.innerHTML = "Dein Grundumsatz beträgt: " + menCookies() + " kcal"
+            outPutCookies.innerHTML = "Dein Gesamumsatz beträgt: " + menCookies() * 2.2 + " kcal"
+            cookieRatio.innerHTML = cookieRatioMen() + " Kekse "
         } else {
-            outPutKcal.innerHTML = womenCookies() + " Kekse"
-            outPutCookies.innerHTML = womenCookies() * 2.2 + " Kekse"
+            outPutKcal.innerHTML = "Dein Grundumsatz beträgt: " + womenCookies() + " kcal"
+            outPutCookies.innerHTML = "Dein Gesamumsatz beträgt: " + womenCookies() * 2.2 + " kcal"
+            cookieRatio.innerHTML = cookieRatioWomen() + " Kekse "
         }
     } else {
         outPutCookies.innerHTML = "Keksemenge kann nicht berechnet werden."
@@ -75,8 +82,14 @@ let giveMeCookies = () => {
 
 }
 let menCookies = () => {
-    return ((664.7 + (13.7 * weight.value) + (5 * height.value) - (6.8 * age.value)).toFixed(2))
+    return (664.7 + (13.7 * weight.value) + (5 * height.value) - (6.8 * age.value)).toFixed(2)
 }
 let womenCookies = () => {
-    return ((655.1 + (9.6 * weight.value) + (1.8 * height.value) - (4.7 * age.value).toFixed(2)))
+    return (655.1 + (9.6 * weight.value) + (1.8 * height.value) - (4.7 * age.value).toFixed(2))
+}
+let cookieRatioMen = () => {
+    return (menCookies() / 60).toFixed(2)
+}
+let cookieRatioWomen = () => {
+    return (womenCookies() / 60).toFixed(2)
 }
